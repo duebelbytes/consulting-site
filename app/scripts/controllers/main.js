ce703851e5f8a91a5a417545f31eadfc3a3d0b35
Bobby Duebelbeis <bduebelbeis@duebelmac.local>
2014-02-16 18:11:45 -0600

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cleverApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
